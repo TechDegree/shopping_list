08682a4f89ac422ae625e4aabd482417b878e978
neex-io <neex.io@protonmail.com>
2017-12-29 05:50:48 -0800

    Add helper menu. Version 2

--- shopping_list.py
+++ shopping_list.py
@@ -15,7 +15,18 @@ while True:
     # quit adding if DONE
     if new_item == 'DONE':
         break
-
+    # show shopping list
+    elif new_item == "SHOW":
+        for item in shopping_list:
+            print(item)
+        continue
+    # show HELP menu
+    elif new_item == "HELP":
+        print("Enter 'DONE' to stop adding items.")
+        print("Enter 'SHOW' to show what is in your list.")         
+        print("Enter 'HELP' to show this menu.")         
+        continue
+    
     # add new item to the list
     shopping_list.append(new_item)
 
