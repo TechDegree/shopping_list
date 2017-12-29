4f5909f103978dd4f0e14cbc8bcb08387eb4b51f
neex-io <neex.io@protonmail.com>
2017-12-29 09:02:54 -0800

    Pick random word, and ask for a guess

--- 3_letter_guessing game/lerret_guess.py
+++ 3_letter_guessing game/lerret_guess.py
@@ -1,6 +1,54 @@
+import random
+import string
+import math
+
 # make a list of word
-# pick a random word
-# draw spaces
-# take a guess
-# draw guessed letters and strikes
-# print out win/lose
+words = [
+    "apple",
+    "banana",
+    "orange",
+    "coconut",
+    "strawberry",
+    "lime",
+    "grapefruit",
+    "lemon",
+    "kumquat",
+    "blueberry",
+    "melon",
+    "raspberry",
+    "avocado",
+    "squash",
+    "pineapple"
+]
+
+# game loop
+while True:
+    start = input("Press enter/return to start, or enter Q to quit\n>>> ")
+    if start.lower() == "q":
+        break
+
+    # pick a random word
+    random_number = random.randint(0, len(words)-1)
+    random_word = words[random_number]
+
+    # round up, so you can always be wrong at least 1 time
+    max_wrong_guesses = math.ceil(len(random_word // 3)) 
+    wrong_guesses = 0
+
+    # draw spaces for the word 
+    print("_ " * len(random_word))
+
+    # take a guess (letter)
+    alphabat = string.ascii_lowercase
+    guess = input("Please choose a letter from English alphabet").lower()
+
+    # draw guessed letters and strikes or add counter if error
+    for letter in random_word:
+        if letter == guess:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+            
+   
+    # give max number of wrong guesses based on len of the word
+    # print out win/lose
