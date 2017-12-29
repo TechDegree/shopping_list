374d6f785c2134bd67c2cd756690750e79ba3f21
neex-io <neex.io@protonmail.com>
2017-12-29 08:03:06 -0800

    Add basic functionality

--- 1_number_guess/number_guess.py
+++ 1_number_guess/number_guess.py
@@ -2,3 +2,16 @@
 # get a number from the player 
 # compare guess and secret number  
 # prit hit/miss
+
+import random 
+ 
+secret_number = random.randint(1, 10) 
+ 
+while True: 
+    guess = int(input("What is your guess?\n>>> ")) 
+ 
+    if guess == secret_number: 
+        print("Correct! {} is the secret number!".format(guess)) 
+        break 
+    else: 
+        print("I am sorry, {} is not the correct number".format(guess))
