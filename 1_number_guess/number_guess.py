be855de27645a840c66672f346435fa4c65c0985
neex-io <neex.io@protonmail.com>
2017-12-29 08:04:27 -0800

    Add comments

--- 1_number_guess/number_guess.py
+++ 1_number_guess/number_guess.py
@@ -1,20 +1,14 @@
-# generate a random number between 1 and 10
-# get a number from the player
-# compare guess and secret number 
-# prit hit/miss
-# max 5 guesses
-
 import random
 
 def game():
-    secret_number = random.randint(1, 10)
+    secret_number = random.randint(1, 10) # generate a random number between 1 and 10
     guesses = []
 
-    while len(guesses) < 5:
-        try:
-            guess = int(input("What is your guess?\n>>> "))
+    while len(guesses) < 5: #limit guesses to 5
+        try: 
+            guess = int(input("What is your guess?\n>>> ")) # get a number from the player
         except ValueError:
-            print("This is a NUMBER guessing game. Please give me a number!")
+            print("This is a NUMBER guessing game. Please give me a number!") # catch non numbers
         else:   
          
             if guess == secret_number:
@@ -26,6 +20,7 @@ def game():
     else:
         print("You run out of guesses. My secret number was {}.".format(secret_number))
 
+    # let player play again
     play_again = input("\nDo you want to play again? Y/n?\n>>> ")
     if play_again.lower() != "n":
         game()
